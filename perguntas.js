3528269c456941fa0cf775e8e5ebef512047d550
TEGARYALDO <jonasbie@prof.educacao.sp.gov.br>
2024-11-14 17:39:46 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,20 @@
+criaCartao(
+    'Programação',
+    'O que é JavaScript?',
+    'O javaScript é uma linguagem de programação'
+)
+criaCartao(
+    'Geografia',
+    'Qual a capital de Portugal?',
+    'Acapital de Portugal é Lisboa'
+)
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'Acapital da França é Paris'
+)
+criaCartao(
+    categoria,
+    pergunta,
+    reposta
+)
